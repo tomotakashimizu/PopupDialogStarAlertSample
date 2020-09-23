fe66d0731b64b1b8d156de4b9fe874b292b99a03
tomotakashimizu <tomotaka.developer@gmail.com>
2020-09-24 02:33:47 +0900

    Initial Commit

---
+++ PopupDialogStarAlertSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopupDialogStarAlertSample
+//
+//  Created by 清水智貴 on 2020/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
